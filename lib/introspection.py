8611cb705304d0f9b0337a28faa8c1869b6495d5
R-y-a-n8 <ryan.kinuthia@student.moringaschool.com>
2024-09-12 15:07:16 +0300

    object

---
+++ lib/introspection.py
@@ -0,0 +1,8 @@
+from vehicle import Vehicle
+from car import Car
+
+print("Car.__bases__:", Car.__bases__)
+print("int.__bases__:", int.__bases__)
+
+print("Car.__class__:", Car.__class__)
+print("int.__class__:", int.__class__)
